72e1434a624ce42aebe31cc6b4214115bbc8f3f2
vladik234 <vlad.stavrov2004.stavrov@mail.ru>
2024-10-20 21:40:49 +0300

    feat: use Reddis for update RegexList and EndpointList

--- src/main/java/com/example/supportfilterservice/flink/SupportRequestFilterJob.java
+++ src/main/java/com/example/supportfilterservice/flink/SupportRequestFilterJob.java
@@ -1,39 +1,46 @@
 package com.example.supportfilterservice.flink;
 
 import com.example.supportfilterservice.config.AppConfig;
+import com.example.supportfilterservice.config.AppConfig.RegexConfig;
 import com.example.supportfilterservice.domain.repository.SensitiveDataRepository;
-import com.example.supportfilterservice.flink.function.CustomFilterFunction;
 import com.example.supportfilterservice.flink.function.RequestProcessor;
-import com.example.supportfilterservice.model.SupportRequest;
-import com.example.supportfilterservice.util.ConfigUtil;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.example.supportfilterservice.service.EndpointService;
+import com.example.supportfilterservice.service.RegexConfigService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.*;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 public class SupportRequestFilterJob {
 
     private final StreamExecutionEnvironment env;
 
+
+    private final RegexConfigService regexConfigService;
+    private final EndpointService endpointService;
     private final AppConfig appConfig;
     private final SensitiveDataRepository sensitiveDataRepository;
+    private final AtomicReference<List<AppConfig.RegexConfig>> regexConfigs = new AtomicReference<>(new ArrayList<>());
+    private final AtomicReference<List<String>> disabledEndpoints = new AtomicReference<>(new ArrayList<>());
 
-    public SupportRequestFilterJob(StreamExecutionEnvironment env, AppConfig appConfig, SensitiveDataRepository sensitiveDataRepository) {
+    public SupportRequestFilterJob(StreamExecutionEnvironment env, RegexConfigService regexConfigService, EndpointService endpointService, AppConfig appConfig, SensitiveDataRepository sensitiveDataRepository) {
         this.env = env;
+        this.regexConfigService = regexConfigService;
+        this.endpointService = endpointService;
         this.appConfig = appConfig;
         this.sensitiveDataRepository = sensitiveDataRepository;
+        loadInitialConfigs();
+        startRedisListener();
     }
 
     public void execute() throws Exception {
@@ -53,26 +60,42 @@ public class SupportRequestFilterJob {
 
         ObjectMapper objectMapper = new ObjectMapper();
 
-//        List<String> disabledEndpoints = appConfig.getEndpoint().getDisabledEndpoints();
-        List<String> disabledEndpoints = Arrays.asList(
-                "https://example.com/api/endpoint1",
-                "https://example.com/api/endpoint2",
-                "https://example.com/api/endpoint3",
-                "https://example.com/api/endpoint4",
-                "https://example.com/api/endpoint5"
-        );
+//
 
 
 
-        List<AppConfig.RegexConfig> regexConfigs = new ArrayList<>();
+
+
 
         env.addSource(consumer)
-                .map(new RequestProcessor(disabledEndpoints, regexConfigs, sensitiveDataRepository, objectMapper))
+                .map(new RequestProcessor(disabledEndpoints.get(), regexConfigs.get(), sensitiveDataRepository, objectMapper))
                 .filter(request -> request != null)
                 .map(objectMapper::writeValueAsString)
                 .addSink(producer);
         // TODO Сохранять отфильтрованное в БД
         env.execute("Support Request Filter Job");
+    }
+    private void loadInitialConfigs() {
+        regexConfigs.set(regexConfigService.getRegexConfigs());
+        disabledEndpoints.set(endpointService.getDisabledEndpoints());
+    }
+
+    private void startRedisListener() {
+        new Thread(() -> {
+            RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+
+            // Слушатель для обновления regexConfigs
+            container.addMessageListener((message, pattern) -> {
+                regexConfigs.set(regexConfigService.getRegexConfigs());
+            }, new ChannelTopic("regexConfigUpdates"));
+
+            // Слушатель для обновления disabledEndpoints
+            container.addMessageListener((message, pattern) -> {
+                disabledEndpoints.set(endpointService.getDisabledEndpoints());
+            }, new ChannelTopic("disabledEndpointsUpdates"));
+
+            container.start();
+        }).start();
     }
 
 }
