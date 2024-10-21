5697733c035d25eae0fdc43227014fee42642b10
vladik234 <vlad.stavrov2004.stavrov@mail.ru>
2024-10-22 00:01:35 +0300

    feat: add ChannelTopics to config

--- src/main/java/com/example/supportfilterservice/config/RedisConfig.java
+++ src/main/java/com/example/supportfilterservice/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 
 import java.util.List;
 
@@ -16,5 +17,14 @@ public class RedisConfig {
         RedisTemplate<String, List<RegexConfig>> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         return template;
+    }
+    @Bean
+    public ChannelTopic disabledEndpointsUpdatesTopic() {
+        return new ChannelTopic("disabledEndpointsUpdates"); // Название канала
+    }
+
+    @Bean
+    public ChannelTopic regexConfigUpdatesTopic() {
+        return new ChannelTopic("regexConfigUpdates"); // Название канала для regexConfig
     }
 }
