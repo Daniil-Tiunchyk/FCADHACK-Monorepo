4a42ac03be1c2542432c71b80d2d0fb338a04f5d
N3zZa <nazar.developer007@gmail.com>
2024-10-25 12:16:01 +0300

    авторизация

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './AuthPage.css'
 import AuthInput from './components/AuthInput/AuthInput'
 import Button from '../../components/Button/Button.jsx'
 import axios from 'axios'
 import { Link, useNavigate } from "react-router-dom";
+
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AuthPage = () => {
   const [login, setLogin] = useState("");
@@ -11,12 +15,84 @@ const AuthPage = () => {
   const navigate = useNavigate();
 
 
-const fetchURLReg = "http://193.22.147.81:8765/api/identity/registration";
-const fetchURLAuth = "http://193.22.147.81:8765/api/identity/auth";
+const fetchURLReg = "http://193.22.147.81:8083/api/identity/registration";
+const fetchURLAuth = "http://193.22.147.81:8083/api/identity/auth";
 
 
-const onAuth = () => {
+const onAuth = (e) => {
+  e.preventDefault();
+  if (password !== "" && login !== "") {
+      axios
+        .post(
+          fetchURLReg,
+          {
+            username: login,
+            password: password,
+            confirmPassword: password,
+            role: {
+              name: "User",
+            },
+          },
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          axios
+            .post(
+              fetchURLAuth,
+              {
+                username: login,
+                password: password,
+              },
+              {
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                },
+              }
+            )
+            .then((response) => {
+               toast.success("Получилось!");
+               navigate("/");
+            })
+            .catch((error) => {
+              toast.error("Ошибка");
+              console.error(error);
+            });
+        })
+        .catch((error) => {
+          axios
+            .post(
+              fetchURLAuth,
+              {
+                username: login,
+                password: password,
+              },
+              {
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                },
+              }
+            )
+            .then((response) => {
+              toast.success("Получилось!");
+              navigate("/");
+            })
+            .catch((error) => {
+              toast.error("Ошибка");
+              console.error(error);
+            });
+        });
+  } else {
+    toast.error("Введите данные")
+  }
 
+    
 }
 
   return (
@@ -58,11 +134,12 @@ const onAuth = () => {
                   </div>
                   <span>Забыли пароль?</span>
                 </div>
-                <Button>Войти</Button>
+                <Button onClick={(e) => onAuth(e)}>Войти</Button>
               </form>
             </div>
           </div>
         </div>
+        <ToastContainer />
         <div className="authImage_block">
           <img src="./men.svg" alt="menimage" />
         </div>
